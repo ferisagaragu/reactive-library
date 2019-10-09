3bb046ec3b1165eb576a9b8a951a1bd2b98ae329
Fernando Isaías García Aguirre <ferisagaragu@gmail.com>
2019-10-09 13:08:14 -0500

    init commit

---
+++ reactive-library/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+class Fancy extends React.Component {
+  render() {
+    return (
+      <Button
+        variant="outline-dark"
+      >
+        Jjajajajajaja la maldad! 
+      </Button>
+    );
+  }
+}
+
+export default Fancy;
---
+++ reactive-library/webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    // LA LINEA DE AQUI ABAJO ES LA MAS IMPORTANTE!
+    // :mindblow: Perdí mas de 2 dias hasta darme cuenta que esta es la linea mas importante de toda esta guia.
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+  externals: {
+    // La linea de aqui abajo es solo para indicar que vamos a utilizar la dependencia "React" de parent-testing-project.
+    'react': 'commonjs react',
+    'react-bootstrap': 'commonjs react-bootstrap'
+  }
+};
