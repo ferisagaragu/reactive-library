5547ee8445e745c5dd85af7debec16f1e1876f60
Fernando Isaías García Aguirre <ferisagaragu@gmail.com>
2019-10-16 18:06:56 -0500

    test data add

--- test/src/config/app.config.js
+++ test/src/config/app.config.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { reducers, initState } from '../declarations/redux.declarations';
-import thunk from 'redux-thunk';
 import { initializeFirebaseApp } from 'reactive';
+import thunk from 'redux-thunk';
 
 //REDUX
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
