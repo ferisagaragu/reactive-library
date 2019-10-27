c41378d8c2ff2b73cc1fd2a4c47f07783af5e0e4
Fernando García <ferisagaragu@gmail.com>
2019-10-27 14:26:05 -0600

    Updates

---
+++ precache-manifest.9414f382e49bfa9f7eb96fe361d9df6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bc6231c189af02907e9835a229d266",
+    "url": "/index.html"
+  },
+  {
+    "revision": "538917d4a60fb407b3b8",
+    "url": "/static/css/2.24e783f3.chunk.css"
+  },
+  {
+    "revision": "7e80ae3018b14386a40f",
+    "url": "/static/css/main.7d5657f0.chunk.css"
+  },
+  {
+    "revision": "538917d4a60fb407b3b8",
+    "url": "/static/js/2.a89d1a2f.chunk.js"
+  },
+  {
+    "revision": "7e80ae3018b14386a40f",
+    "url": "/static/js/main.91d2105a.chunk.js"
+  },
+  {
+    "revision": "03f01d85529e34b56f3e",
+    "url": "/static/js/runtime-main.f02e343f.js"
+  }
+]);
