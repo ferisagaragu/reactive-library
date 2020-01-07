09b5a17903ef5ce553a2bd5cdf537a07de723116
Fernando García <ferisagaragu@gmail.com>
2020-01-07 13:23:31 -0600

    update datas

--- command/.reactive-data/reactive-update.bat
+++ command/.reactive-data/reactive-update.bat
@@ -1,3 +1,6 @@
+SET baseDir=%CD%
+cd C:\.reactive-data
 curl https://raw.githubusercontent.com/ferisagaragu/reactive-library/master/command/.reactive-data/command.jar -O command.jar
+cd %baseDir%
 echo Update success
 reactive
