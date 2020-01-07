ced190d617c47cad0c159686d09bc79af48828ff
Fernando García <ferisagaragu@gmail.com>
2020-01-07 11:36:07 -0600

    new cmd data

--- command/.reactive-data/start.bat
+++ command/.reactive-data/start.bat
@@ -1,5 +1,6 @@
 SET exeDir=%CD%
 cd C:\.reactive-data
+curl https://raw.githubusercontent.com/ferisagaragu/reactive-library/master/command/.reactive-data/command.jar -O command.jar
 cls
 java -jar command.jar %exeDir%
 cd %exeDir%
